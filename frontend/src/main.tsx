a107a2a41c83e8056439d15c66ecd002228277d0
khaoulakhmi <lakhmikhaoula@gmail.com>
2026-03-18 10:53:43 +0100

    feat: replace App component with RouterProvider for improved routing management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
 import { AppProviders } from "./app/providers/AppProviders"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./app/router"
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProviders>
-      <App />
+      <RouterProvider router={router} />
     </AppProviders>
   </React.StrictMode>
 )
