4c9a9f2e92340fad3174ec88cac6b51bf66c4599
khaoulakhmi <lakhmikhaoula@gmail.com>
2026-03-31 14:41:30 +0200

    feat: update greeting message in root endpoint and organize imports in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
-
+import userRouter from "./modules/user/user.routes";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", userRouter);
 app.get("/", (req: Request, res: Response) => {
-  res.send("API is running...");
+  res.send("hello khaoula, API is running...");
 });
 
 export default app;
