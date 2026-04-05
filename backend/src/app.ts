80dd2c6a274fa51255b63927bcc72d5bee39287f
khaoulakhmi <lakhmikhaoula@gmail.com>
2026-04-05 15:53:51 +0200

    feat: update dependencies and add authentication to user routes
    - Updated devDependencies in package.json, including @types/bcrypt and @types/jsonwebtoken.
    - Upgraded @prisma/adapter-pg and added zod as a new dependency.
    - Configured CORS to allow requests from http://localhost:5173.
    - Added authRouter for authentication routes.
    - Secured user retrieval route with authentication and authorization middleware for admin access.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,16 @@ import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
 import userRouter from "./modules/user/user.routes";
+import authRouter from "./modules/auth/auth.routes";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRouter);
+app.use('/api/auth', authRouter)
 app.get("/", (req: Request, res: Response) => {
   res.send("hello khaoula, API is running...");
 });
--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { authenticate } from "../../middlewares/auth.middleware";
+import { authorize } from "../../middlewares/role.middleware";
+import { Role } from "../../generated/prisma/enums";
 
 
 const userRouter = Router();
 
-userRouter.get("/", UserController.getAllUsers);
+userRouter.get("/", authenticate, authorize(Role.ADMIN), UserController.getAllUsers);
 userRouter.get("/:username", UserController.getUserByUsername);
 userRouter.get("/id/:id", UserController.getUserById);
 userRouter.post("/", UserController.createUser);
