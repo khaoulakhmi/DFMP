4d30c286042f6a1ab7d13d822635f754b875d404
khaoulakhmi <lakhmikhaoula@gmail.com>
2026-04-06 10:34:04 +0200

    feat: replace RouterProvider with QueryClientProvider for improved data fetching

--- frontend/src/app/providers/AppProviders.tsx
+++ frontend/src/app/providers/AppProviders.tsx
@@ -1,18 +1,28 @@
 import {type ReactNode } from "react"
 import { ChakraProvider } from "@chakra-ui/react"
-import { BrowserRouter } from "react-router-dom"
 import { system } from "../../shared/theme"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 
 interface Props {
   children?: ReactNode
 }
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            refetchOnWindowFocus: false
+        }
+    }
+})
+
 export const AppProviders = ({ children }: Props) => {
   return (
     <ChakraProvider value={system}>
-      <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         {children}
-      </BrowserRouter>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { AppProviders } from "./app/providers/AppProviders"
-import { RouterProvider } from "react-router-dom"
-import { router } from "./app/router"
+import App from "./App"
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProviders>
-      <RouterProvider router={router} />
+      <App />
     </AppProviders>
   </React.StrictMode>
 )
