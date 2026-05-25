6dfe7dba90fd39545ffa22b849d9f6bfe817b8ed
khaoulakhmi <lakhmikhaoula@gmail.com>
2026-05-25 12:59:55 +0200

    feat: add authentication and authorization middleware to provider routes

--- backend/src/modules/provider/provider.routes.ts
+++ backend/src/modules/provider/provider.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { ProviderController } from "./provider.controller";
+import { authenticate } from "../../middlewares/auth.middleware";
+import { authorize } from "../../middlewares/role.middleware";
+import { Role } from "../../generated/prisma/enums";
 
 
 
 const providerRouter = Router();
 
-providerRouter.get("/", ProviderController.getAllProviders);
-providerRouter.get("/:id", ProviderController.getProviderById);
-providerRouter.post("/", ProviderController.createProvider);
-providerRouter.put("/:id", ProviderController.updateProvider);
-providerRouter.delete("/:id", ProviderController.deleteProvider);
+providerRouter.get("/", authenticate, authorize(Role.ADMIN, Role.SALES), ProviderController.getAllProviders);
+providerRouter.get("/:id", authenticate, authorize(Role.ADMIN, Role.SALES), ProviderController.getProviderById);
+providerRouter.post("/", authenticate, authorize(Role.ADMIN, Role.SALES), ProviderController.createProvider);
+providerRouter.put("/:id", authenticate, authorize(Role.ADMIN, Role.SALES), ProviderController.updateProvider);
+providerRouter.delete("/:id", authenticate, authorize(Role.ADMIN, Role.SALES), ProviderController.deleteProvider);
 
 export default providerRouter;
