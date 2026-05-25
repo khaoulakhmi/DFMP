82dea997991e5ab3cbed9e3f5e33b2633a52b7a9
khaoulakhmi <lakhmikhaoula@gmail.com>
2026-05-25 13:50:36 +0200

    feat: add routing for providers, designations, products, and lots; update dependencies and tsconfig

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,10 @@ import { Request, Response } from "express";
 import cors from "cors";
 import userRouter from "./modules/user/user.routes";
 import authRouter from "./modules/auth/auth.routes";
+import providerRouter from "./modules/provider/provider.routes";
+import designationRouter from "./modules/designation/designation.routes";
+import { ProductRouter } from "./modules/product/product.routes";
+import LotRouter from "./modules/lot/lot.routes";
 const app = express();
 
 app.use(cors({
@@ -12,6 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRouter);
 app.use('/api/auth', authRouter)
+app.use("/api/providers", providerRouter);
+app.use("/api/designations", designationRouter);
+app.use("/api/products", ProductRouter);
+app.use("/api/lots", LotRouter);
 app.get("/", (req: Request, res: Response) => {
   res.send("hello khaoula, API is running...");
 });
